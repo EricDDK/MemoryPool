d298c110bb3234f6664e94b40476cb33e729dfb6
dingdekai <dekai.ding@shen021.com>
2019-06-14 18:05:14 +0800

    [Add] test case about large memory segment.

--- test.cpp
+++ test.cpp
@@ -33,6 +33,26 @@ public:
 	double d;
 };
 
+class TestLarge
+{
+public:
+	TestLarge(int a)
+	{
+		l1 = a;
+	}
+	long long l1; long long l2; long long l3; long long l4;
+	long long l5; long long l6; long long l7; long long l8;
+	long long l9; long long l10; long long l11; long long l12;
+	long long l13; long long l14; long long l15; long long l16;
+	long long l17; long long l18; long long l19; long long l20;
+	long long l21; long long l22; long long l23; long long l24;
+	long long l25; long long l26; long long l27; long long l28;
+	long long l29; long long l30; long long l31; long long l32;
+	long long l33; long long l34; long long l35; long long l36;
+	long long l37; long long l38; long long l39; long long l40;
+	long long l41; long long l42; long long l43; long long l44;
+};
+
 void test1()
 {
 	MemoryPool m;
@@ -51,10 +71,19 @@ void test2()
 	m.safeFree(p);
 }
 
+void testLarge()
+{
+	MemoryPool m;
+	auto p = m.safeMalloc<TestLarge>(5);
+	EXPECT(p->l1, 5);
+	m.safeFree(p);
+}
+
 int main()
 {
 	test1();
 	test2();
+	testLarge();
 
 #ifdef _MSC_VER
 	//_CrtDumpMemoryLeaks();
